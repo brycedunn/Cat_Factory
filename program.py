80c98a350b8e106844a2e2c37a2d0b17da1c5f88
Bryce <34713702+brycedunn@users.noreply.github.com>
2018-06-01 10:46:14 -0400

    Add files via upload

---
+++ program.py
@@ -0,0 +1,66 @@
+# 30 May 2018
+
+"""
+This program is a LOLcat factory that uses data from an online cat service to populate the app.
+It will then display cats to user.
+"""
+
+import os
+import platform
+import subprocess
+import cat_service
+
+
+def main():
+    print_header()
+    folder = output_folder()
+    print('Found or created folder: {}'.format(folder))
+    download_cats(folder)
+    display_cats(folder)
+
+
+def print_header():
+    print('-----------------------')
+    print('      Cat Factory')
+    print('-----------------------')
+
+
+def output_folder():
+    base_folder = os.path.dirname(__file__)
+    folder = 'cat_pictures'
+    full_path = os.path.join(base_folder, folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print('Creating new directory at {}'.format(full_path))
+        os.mkdir(full_path)
+
+    return full_path
+
+
+def download_cats(folder):
+    print('Contacting server to download cats...')
+    cat_count = 8
+    for i in range(1, cat_count+1):
+        name = 'lolcat_{}'.format(i)
+        print('Downloading cat {}'.format(name))
+        cat_service.get_cat(folder, name)
+    print('Done!')
+
+
+def display_cats(folder):
+    """
+    This method will display the cats to the user.
+    :return: display
+    """
+    if platform.system() == 'Darwin':
+        subprocess.call(['open', folder])
+    elif platform.system() == 'Windows':
+        subprocess.call(['explorer', folder])
+    elif platform.system() == 'Linux':
+        subprocess.call(['xdg-open', folder])
+    else:
+        print("We don't support your OS :(")
+
+
+if __name__ == '__main__':
+    main()
